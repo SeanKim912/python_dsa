cedc65553b4ec195d6fb24c6f593b73595b5e679
Sean <seankim912@yahoo.com>
2023-12-16 21:40:11 -0600

    comprehensions

--- lists.py
+++ lists.py
@@ -6,8 +6,20 @@ if preallocated memory is full.
 Dictionaries are equivalent of hash tables. time complexity of O(1). Unordered collection of
 data values stored like a map in key:value pairs.
 
-Tuple is a collection of Python objects similar to a list, but immutable.
+Tuple is a collection of Python objects similar to a list, but immutable and can also contain
+elements of different types
+
+Sets are unordered collections of mutable data with no duplicates. Most commonly used for
+membership testing and eliminating duplicates. Uses hashing for O(1) insertion, deletion,
+traversal. If multiple values are present at same index position, then the value is appended
+to that index position to form a Linked List.
 """
+
+# List comprehension
+input_list = [1, 2, 3, 4, 4, 5, 6, 7, 7]
+list_using_comp = [var for var in input_list if var % 2 == 0]
+print("Output List using list comprehensions:", list_using_comp)
+
 
 Dict = {'Name': 'Geeks', 1: [1, 2, 3, 4]}
 print(Dict.get(1))
@@ -16,3 +28,10 @@ print(Dict.get(1))
 
 myDict = {x: x**2 for x in [1, 2, 3, 4, 5]}
 print(myDict)
+
+list1 = [1, 2, 3, 4, 5]
+Tuple = tuple(list1)
+print(Tuple)
+
+Set = set([1, 2, 'Geeks', 4, 'For', 6, 'Geeks'])
+print(Set)
