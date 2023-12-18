4c9f5643ccdd8dd953a80a1a01208952e794d569
Sean <seankim912@yahoo.com>
2023-12-18 17:32:04 -0600

    classes for linked list defined

--- linked_list.py
+++ linked_list.py
@@ -1,4 +1,4 @@
-Class Node(object):
+class Node(object):
     def __init__(self, val):
         self.val = val
         self.next = None
@@ -14,3 +14,45 @@ Class Node(object):
 
     def set_next(self, next):
         self.next = next
+
+class LinkedList(object):
+    def __init__(self, head = None):
+        self.head = head
+        self.count = 0
+
+        def insert(self, data):
+            new_node = Node(data)
+            new_node.set_next(self.head)
+            self.head = new_node
+            self.count += 1
+
+        def find(self, val):
+            item = self.head
+            while item != None:
+                if item.get_data() == val:
+                    return item
+
+            else:
+                item = item.get_next()
+
+            return None
+
+        def remove(self, item):
+            current = self.head
+            previous = None
+
+            while current is not None:
+                if current.data == item:
+                    break
+
+                previous = current
+                current = current.get_next()
+
+            if current is None:
+                raise ValueError(f"{item} is not in the list")
+            if previous is None:
+                self.head = current.next
+                self.count -= 1
+            else:
+                previous.set_next(current.get_next())
+                self.count -= 1
